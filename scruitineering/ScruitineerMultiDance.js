50310367549217d6aa70f577f5a4df7a15530624
dshefman <drew.shefman@mlb.com>
2017-01-21 20:24:15 -0600

    Rule 10

--- scruitineering/ScruitineerMultiDance.js
+++ scruitineering/ScruitineerMultiDance.js
@@ -20,6 +20,7 @@
     }
 })( this, function( _, Util) {
 
+    const RULE_10 = '10';
 
     var ScruitineerMultiDance = function(){};
     ScruitineerMultiDance.prototype.toString = function(){return 'ScruitineerMultiDance'};
@@ -36,11 +37,13 @@
         var countedPlacementsPerDancer = Util.countPlacementsPerDancer(placementsByDancer);
         console.log('countedPlacementsPerDancer', countedPlacementsPerDancer)
 
-        var ranking = placeDancers(sortedSummation, countedPlacementsPerDancer);
+        var notes = {};
+        var ranking = placeDancers(sortedSummation, countedPlacementsPerDancer, notes);
 
         var rtn = {
             dancePlacements: dancePlacements,
             summation: placementSummationByDancer,
+            notes: notes,
             ranking: ranking
         };
         console.log('results', JSON.stringify(rtn, null, 4));
@@ -55,51 +58,25 @@
         return results
     }
 
-    function placeDancers (sortedSummation, countedPlacementsPerDancer){
+    function placeDancers (sortedSummation, countedPlacementsPerDancer, notes){
         var ranking = {}
         var rank = 1;
+        //Sort ascending the summation of each of the competitors dance placements
         _.each(_.orderBy(_.keys(sortedSummation), _.toNumber, ['asc']), function(key, index){
             var sameSumDancers = _.get(sortedSummation, key);
-            console.log('sameSumDancer', sameSumDancers);
+            console.log('sameSumDancer',rank,  sameSumDancers);
             if (_.size(sameSumDancers) == 1) {
+                //There is one clear winner, place them
                 rankDancer(ranking, rank, _.first(sameSumDancers))
                 rank++;
             } else {
                 console.log('summation tie for ', key, sameSumDancers);
-                var tieBreakingPlacements = []
+                var tieBreakingPlacements = [];
                 _.each(sameSumDancers, function(dancer){
-                    tieBreakingPlacements.push(_.merge(_.clone(_.get(countedPlacementsPerDancer, dancer)), {'dancer':dancer}));
+                    tieBreakingPlacements.push(_.merge(_.clone(_.get(countedPlacementsPerDancer, dancer)), {'dancer':dancer})); //Convert {dancerX:{1:1, 2:1} to [{1:1, 2:1, dancer:X}]
                 })
-                var possiblePlaces = rank + (_.size(tieBreakingPlacements) - 1);
-                var iterCount = 0;
-                while(rank < possiblePlaces && iterCount < 20) {
-                    console.log('tieBreakingPlacements', tieBreakingPlacements, possiblePlaces)
-                    var findHighestPlacements = _.orderBy(_.map(tieBreakingPlacements, function (countedPlacement) {
-                        var highestPlacements = _.pickBy(countedPlacement, function (value, key) {
-                            return key <= rank || key == 'dancer'
-                        })
-                        console.log('highestPlacements', highestPlacements)
-                        var sumCount = {sum: 0, dancer: '', highestRank: 100}
-                        _.each(highestPlacements, function (value, key) {
-                            if (key == 'dancer') {
-                                _.set(sumCount, 'dancer', value)
-                            }
-                            else {
-                                sumCount.sum = sumCount.sum + value;
-                                if (key < sumCount.highestRank) {
-                                    sumCount.highestRank = _.toNumber(key);
-                                }
-                            }
-                        })
-                        return sumCount;
-                    }), ['highestRank', 'sum'], ['asc', 'desc'])
-                    console.log('findHighestPlacements', findHighestPlacements)
-                    _.each(findHighestPlacements, function (sumDancerObj) {
-                        rankDancer(ranking, rank, sumDancerObj.dancer);
-                        rank++;
-                    })
-                    iterCount++
-                }
+                rank = breakTie(tieBreakingPlacements, countedPlacementsPerDancer, ranking, rank,notes, 0);
+
             }
 
         })
@@ -109,9 +86,71 @@
         return ranking
     }
 
+    function breakTie(tiedPlacements, countedPlacements, ranking, lookingForRankPosition, notes, iteration){
+        console.log('break tie('+iteration +') for place:' + lookingForRankPosition, tiedPlacements);
+
+       addNotesAboutWhatRuleIsUsed(notes, tiedPlacements, RULE_10);
+
+        var findHighestPlacements = computeFindHighestPlacements(tiedPlacements, countedPlacements, lookingForRankPosition);
+        console.log('findHighestPlacements',lookingForRankPosition, findHighestPlacements);
+        //Compare only the first two, even if there are more then two that are tied. Since they are sorted, if [0] > [1] then we know there isn't a tie
+        var dancerPlacements1 = findHighestPlacements[0];
+        var dancerPlacements2 = findHighestPlacements[1];
+        if (!dancerPlacements2){
+            lookingForRankPosition = rankDancer(ranking, lookingForRankPosition, dancerPlacements1.dancer);
+
+        } else {
+            var hasGTERanks = dancerPlacements1.highestRank <= dancerPlacements2.highestRank;
+            var hasSameRanksGreaterCounts = dancerPlacements1.highestRank == dancerPlacements2.highestRank && dancerPlacements1.count > dancerPlacements2.count
+            console.log('hasGTERanks', hasGTERanks, 'hasSameRanksGreaterCounts', hasSameRanksGreaterCounts)
+            if (hasGTERanks || hasSameRanksGreaterCounts){
+                lookingForRankPosition = rankDancer(ranking, lookingForRankPosition, dancerPlacements1.dancer);
+                lookingForRankPosition = removePlacedDancerAndContinueToBreakTie(tiedPlacements, dancerPlacements1.dancer, countedPlacements, ranking, lookingForRankPosition, notes, iteration)
+            }
+        }
+        return lookingForRankPosition;
+
+    }
+
+    function removePlacedDancerAndContinueToBreakTie(tiedPlacements, dancer, countedPlacements, ranking, lookingForRankPosition, notes, iteration){
+        tiedPlacements = _.reject(tiedPlacements, {dancer: dancer});
+        if (_.size(tiedPlacements)!=0) {
+            lookingForRankPosition = breakTie(tiedPlacements, countedPlacements, ranking, lookingForRankPosition, iteration+1)
+        }
+        return lookingForRankPosition
+    }
+
+    function addNotesAboutWhatRuleIsUsed(notes, collection, rule){
+        _.each(collection, function(dancerObj){
+            notes[_.get(dancerObj, 'dancer')] = rule;
+        });
+    }
+
+    function computeFindHighestPlacements(tiedPlacements, countedPlacements, rank){
+        var findHighestPlacements = _.orderBy(_.map(tiedPlacements, function (countedPlacement) {
+            var highestPlacements = _.pickBy(countedPlacement, function (value, key) {
+                return key <= rank || key == 'dancer'
+            })
+            console.log('highestPlacements', highestPlacements)
+            var sumCount = {count: 0, dancer: '', highestRank: 100}
+            _.each(highestPlacements, function (value, key) {
+                if (key == 'dancer') {
+                    _.set(sumCount, 'dancer', value)
+                }
+                else {
+                    sumCount.count = sumCount.count + value;
+                    sumCount.highestRank = _.max([rank, _.toNumber(key)])
+                }
+            })
+            return sumCount;
+        }), ['highestRank', 'count'], ['asc', 'desc'])
+        return findHighestPlacements
+    }
+
     function rankDancer( ranking, rank, dancer) {
         console.log('rankDancer', rank, dancer)
         ranking[rank] = dancer;
+        return rank +1;
     }
 
     return ScruitineerMultiDance;
--- test/specs/scruitineering/ScrutineerMultiDanceSpec.js
+++ test/specs/scruitineering/ScrutineerMultiDanceSpec.js
@@ -38,7 +38,7 @@ define([
 
         }
 
-        describe('Rule 9', function() {
+        describe('Rule 9: For a multidance, the dancer with the lowest summative score is placed highest', function() {
             it('Multidance', function () {
                 var dancers = [91, 92, 93, 94, 95, 96, 97, 98];
                 var A = createDanceResults('W', dancers, [1, 4, 2, 5, 3, 6, 7, 8]);
@@ -64,8 +64,8 @@ define([
             })
         })
 
-            describe('Rule 10', function(){
-            it('Multidance', function(){
+        describe('Rule 10: When there is a tie in the sums, then count the number of N through place to break the tie', function(){
+            it('Multidance 2x2 way summative tie', function(){
                 var dancers = [101,102,103,104,105,106];
                 var A = createDanceResults('W', dancers, [1,2,6,5,4,3]);
                 var B = createDanceResults('T', dancers, [1,2,4,3,5,6]);
@@ -79,13 +79,18 @@ define([
                 assert.deepEqual(_.get(results, 'summation.105'), 14,'5th place couple');
                 assert.deepEqual(_.get(results, 'summation.106'), 15,'6th place couple');
 
+                assert.deepEqual(_.get(results, 'notes.101'), '10' ,'101 rule 10');
+                assert.deepEqual(_.get(results, 'notes.102'), '10' ,'102 rule 10');
+                assert.deepEqual(_.get(results, 'notes.103'), '10' ,'103 rule 10');
+                assert.deepEqual(_.get(results, 'notes.104'), '10' ,'104 rule 10');
+
 
                 var rankingExpectation ={1:'101', 2:'102', 3:'103', 4:'104', 5:'105', 6:'106'};
                 assert.deepEqual(_.get(results, 'ranking'), rankingExpectation, 'final placements')
 
             })
 
-            it('Multidance, tough tie breaker', function(){
+            it('Multidance, 4 way summative tie + 2 way summative tie', function(){
                 var dancers = [201,202,203,204,205,206];
                 var A = createDanceResults('W', dancers, [1,6,2,3,5,4]);
                 var B = createDanceResults('T', dancers, [6,2,1,4,3,5]);
@@ -100,6 +105,12 @@ define([
                 assert.deepEqual(_.get(results, 'summation.205'), 18,'5th place couple');
                 assert.deepEqual(_.get(results, 'summation.206'), 18,'6th place couple');
 
+                assert.deepEqual(_.get(results, 'notes.201'), '10' ,'201 rule 10');
+                assert.deepEqual(_.get(results, 'notes.202'), '10' ,'202 rule 10');
+                assert.deepEqual(_.get(results, 'notes.203'), '10' ,'203 rule 10');
+                assert.deepEqual(_.get(results, 'notes.204'), '10' ,'204 rule 10');
+                assert.deepEqual(_.get(results, 'notes.205'), '10' ,'205 rule 10');
+                assert.deepEqual(_.get(results, 'notes.206'), '10' ,'206 rule 10');
 
                 var rankingExpectation ={1:'201', 2:'202', 3:'203', 4:'204', 5:'205', 6:'206'};
                 assert.deepEqual(_.get(results, 'ranking'), rankingExpectation, 'final placements')
